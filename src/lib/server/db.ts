2b97b426c63381d74ce32cb7e094a9ee450f39d0
Najjad <najjadzeenni19@gmail.com>
2024-06-27 21:27:25 +0400

    Fix: cached connections

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,10 +1,19 @@
 import { SECRET_MONGODB_URI } from "$env/static/private";
 import mongoose from "mongoose";
 
+let cachedConnection: typeof mongoose | null = null;
+
 export async function connect_to_db() {
-	try {
-		return await mongoose.connect(SECRET_MONGODB_URI);
-	} catch (err) {
-		console.log(err);
-	}
+  if (cachedConnection) {
+    return cachedConnection;
+  }
+
+  try {
+    const connection = await mongoose.connect(SECRET_MONGODB_URI);
+    cachedConnection = connection;
+    return connection;
+  } catch (err) {
+    console.log(err);
+    throw err; // Re-throw the error to handle it in your function
+  }
 }
